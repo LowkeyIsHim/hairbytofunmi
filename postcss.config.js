289bd292cebdc89838e897fa0696f8c2c0012bc5
༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻⁩ <152390226+LowkeyIsHim@users.noreply.github.com>
2025-11-26 18:52:37 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
+// postcss.config.js (Old - needs update if using the package directly)
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {}, // <--- This is the source of the error if it's the main package
     autoprefixer: {},
   },
 }
