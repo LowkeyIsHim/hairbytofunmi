87c4ef819bae4225754a0fd5418a59e7906266c7
༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻⁩ <152390226+LowkeyIsHim@users.noreply.github.com>
2025-11-27 13:29:17 +0100

    Create AuthContext.js

---
+++ src/context/AuthContext.js
@@ -0,0 +1,38 @@
+"use client";
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+
+const AuthContext = createContext({});
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {loading ? (
+        <div className="h-screen w-full flex items-center justify-center bg-brand-50">
+           <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-brand-500"></div>
+        </div>
+      ) : (
+        children
+      )}
+    </AuthContext.Provider>
+  );
+};
